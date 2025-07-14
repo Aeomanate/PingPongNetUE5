5a06f68e018b0f59018028ed5b084c11220b307c
Aeomanate <aeomanate@gmail.com>
2025-07-14 23:26:03 +0300

    Standardized paths in `build.bat`, updated Unreal Engine directory, removed redundant `pause` command.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 chcp 65001
 
-set UESourcesPath = D:\Prog\Env\IDE\UE_5.5
-set ClientPath = Build\Client
-set ServerPath = Build\Server
+set UESourcesPath=D:\Prog\Env\Libraries\UnrealEngine
+set ClientPath=Build\Client
+set ServerPath=Build\Server
 
 %UESourcesPath%\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
 -project="%cd%\PingPongNet.uproject" ^
@@ -11,8 +11,6 @@ set ServerPath = Build\Server
 -archivedirectory=%ClientPath% ^
 -stagingdirectory=Stage ^
 -build -cook -package -stage -archive -pak -allmaps -client
-
-pause
 
 %UESourcesPath%\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
 -project="%cd%\PingPongNet.uproject" ^
