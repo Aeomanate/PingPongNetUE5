9e5aa045a10405873b9055a1fa8ad8d7b86b6054
Aeomanate <aeomanate@gmail.com>
2025-07-15 17:11:50 +0300

    Stored to config ip addres for clients and servers

--- start_client1.bat
+++ start_client1.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 set ClientExePath=%cd%\WindowsClient\PingPongNetClient.exe
 
-start "" "%ClientExePath%" -WINDOWED -ResX=640 -ResY=480 -ExecCmds="open 127.0.0.1"
+start "" "%ClientExePath%" -WINDOWED -ResX=640 -ResY=480
--- start_client2.bat
+++ start_client2.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 set ClientExePath=%cd%\WindowsClient\PingPongNetClient.exe
 
-start "" "%ClientExePath%" -WINDOWED -ResX=640 -ResY=480 -WinX=10 -WinY=50 -ExecCmds="open 127.0.0.1"
+start "" "%ClientExePath%" -WINDOWED -ResX=640 -ResY=480 -WinX=10 -WinY=50
